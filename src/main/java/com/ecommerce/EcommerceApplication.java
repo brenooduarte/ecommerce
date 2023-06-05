2bbe025daa298bf720b9a099c935a5d349d68467
Pedro Lucas <pedrolukasneto@gmail.com>
2023-06-05 19:17:56 -0300

    feat: add swagger

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -3,6 +3,8 @@ package com.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 public class EcommerceApplication {
 
---
+++ src/main/java/com/ecommerce/domain/controller/ConfigController.java
@@ -0,0 +1,12 @@
+package com.ecommerce.domain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("/test")
+public class ConfigController {
+
+}
