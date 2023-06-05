df4567d6de68b97767ff85a6732f96b5687f3f56
Pedro Lucas <pedrolukasneto@gmail.com>
2023-06-05 20:34:33 -0300

    feat: add postgresql dependency to pom

--- src/main/java/com/ecommerce/domain/controller/ConfigController.java
+++ src/main/java/com/ecommerce/domain/controller/ConfigController.java
@@ -1,12 +1,24 @@
 package com.ecommerce.domain.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.domain.models.User;
 
 import io.swagger.annotations.Api;
 
 @RestController
 @RequestMapping("/test")
+@Api
 public class ConfigController {
 
+	@GetMapping
+	public ResponseEntity<String> helloWorld() {
+
+		return new ResponseEntity<>(HttpStatus.ACCEPTED).ok("Hello World");
+
+	}
 }
