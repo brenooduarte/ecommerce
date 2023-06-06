77c58077eb5464e4c3c5a2370efb1d9777b640de
Pedro Lucas <pedrolukasneto@gmail.com>
2023-06-05 21:13:31 -0300

    feat: add example of swagger tag

--- src/main/java/com/ecommerce/domain/controller/ConfigController.java
+++ src/main/java/com/ecommerce/domain/controller/ConfigController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/test")
+@Tag(description = "Controller do usuário", name = "User")
 public class ConfigController {
 
 	@GetMapping
